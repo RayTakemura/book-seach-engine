f3b41bf49960c786f0e10da8379972ff7d3868f2
unknown <raytakemura@gmail.com>
2021-07-10 21:32:49 -0700

    fixed and tested the addBook and removeBook

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,68 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+        }
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email,password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+        }
+      }
+      
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($authors: [String], $description: String, $title: String, $bookId: String, $image: String, $link: String) {
+    saveBook(authors: $authors, description: $description, title: $title, bookId: $bookId, image: $image, link: $link){
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+      }
+    }
+  }
+
+`;
+
+
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String) {
+    removeBook(bookId: $bookId){
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+      }
+    }
+  }
+
+`;
